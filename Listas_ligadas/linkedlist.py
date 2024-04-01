0eefe83c97c8d1d41eb43e60d4a849b009563986
ParsivalT <tthiagovinnicius@gmail.com>
2024-03-31 23:29:50 -0300

    add remove

--- Listas_ligadas/linkedlist.py
+++ Listas_ligadas/linkedlist.py
@@ -13,8 +13,6 @@ class LinkedList:
 
     # TODO: create a remove function.
 
-    # TODO: create a index function.
-
     # TODO: create a set function.
 
     # TODO: create a get function.
@@ -27,6 +25,44 @@ class LinkedList:
         else:
             self.tail.next = Node(value)
             self.tail = self.tail.next
+
+    def remove(self, value) -> None:
+        if self.head is None:
+            raise ValueError(f"{value} is not in list")
+
+        elif self.head.data == value:
+            self.head = self.head.next
+            self._size -= 1
+
+        else:
+            ancestor = self.head
+            pointer = self.head.next
+
+            while pointer:
+                if pointer.data == value:
+                    ancestor.next = pointer.next
+                    pointer.next = None
+                    self._size -= 1
+                    return True
+
+                ancestor = pointer
+                pointer = pointer.next
+
+    def index(self, data) -> int:
+        pointer = self.head
+        index = 0
+        if pointer.data == data or self.tail.data == data:
+            return index
+
+        else:
+            while pointer.next:
+                pointer = pointer.next
+                index += 1
+                if pointer.data == data:
+                    return index
+
+            else:
+                raise (IndexError("list index out of range"))
 
     def __repr__(self) -> str:
         r = ""
@@ -45,7 +81,6 @@ class LinkedList:
         return self._size
 
 
-
 class LinkedListNoOptimized:
     """
     Class responsible for managing nodes.
@@ -60,15 +95,16 @@ class LinkedListNoOptimized:
         if pointer is None:
             self.head = Node(value)
 
-        else: 
+        else:
             while pointer.next:
                 pointer = pointer.next
 
             pointer.next = Node(value)
 
 
-
 import time
+
+
 def calculate_execution_time(list, param):
     init = time.time()
     for _ in range(param):
@@ -81,9 +117,9 @@ def calculate_execution_time(list, param):
 
 
 if __name__ == "__main__":
-    samples =  10000
+    samples = 10000
 
-    list1= LinkedList()
+    list1 = LinkedList()
     time_list1 = calculate_execution_time(list1, samples)
 
     print(f"the execution time of the first list was: {time_list1:.6f} seconds")
@@ -91,6 +127,17 @@ if __name__ == "__main__":
     list2 = LinkedListNoOptimized()
     time_list2 = calculate_execution_time(list2, samples)
 
-
     print(f"the execution time of the second list was: {time_list2:.6f} seconds")
 
+    lista = LinkedList()
+
+    lista.append(10)
+    lista.append(9)
+    lista.append(5)
+    lista.append(16)
+
+    print(str(lista))
+
+    lista.remove(5)
+
+    print(str(lista))
